472a1a5f421fe6fad2d8b26d0f012b689aa79f78
Ammara <AMMARA.YASIN.UCL@GMAIL.COM>
2024-12-05 16:20:14 +0000

    Update config.js

--- config/config.js
+++ config/config.js
@@ -60,10 +60,9 @@ export const intro = {
 export const about = {
   title: "Who We Are",
   description: [
-    "I graduated from National University of Sciences and Technology (NUST) in 2020 with a degree in Computer Engineering. I kick started my professional career as a mobile application developer working remotely for a UK based startup — Lessgo.",
+    "We are a group based out of University College London, exploring .... ",
     <img src="path/to/your/image.jpg" alt="Image description" />, // Add an image here
-    "The tech industry is ever-evolving and I love being able to grow with it, while continually solidifying the fundamentals. I opensource my code from a place of empathy - for future developers, teammates, users, and with accessibility in mind. I also love joining communities, helping and mentoring new developers, and supporting organizations to promote diversity in tech. I’m sharing my programming journey on instagram, helping people who are just getting into the space of programming.",
-    "When I’m not programming, I focus on my hobbies which are: diary writing, doodling, meeting people and growing my network.",
+    "More text below image here",
   ],
 };
 
@@ -73,7 +72,7 @@ export const projects = {
   cards: [
     {
       title: "INSERT TITLE",
-      description: "A digital diary and mood tracking app that helps you in keeping track of your mood and productivity throughout the month/year.",
+      description: "Insert description.",
       icons: [
         {
           icon: faAppStore,
@@ -87,7 +86,7 @@ export const projects = {
     },
     {
       title: "INSERT TITLE",
-      description: "An emotional well being and mental health app. The app helps people to navigate their emotions in the light of the Quran.",
+      description: "Insert description",
       icons: [
         {
           icon: faAppStore,
@@ -101,7 +100,7 @@ export const projects = {
     },
     {
       title: "INSERT TITLE",
-      description: "A portfolio website template that helps you showcase your work, projects and skills as a software developer or freelancer.",
+      description: "Insert description",
       icons: [
         {
           icon: faGithub,
@@ -117,12 +116,12 @@ export const work = {
   cards: [
     {
       title: "Event A",
-      description: "I create pixel perfect iOS and Andriod apps using Flutter.",
+      description: "Insert description",
       icons: null,
     },
     {
       title: "Event B",
-      description: "I create responsive static websites using Reactjs.",
+      description: "Insert description",
       icons: null,
     }
   ],
@@ -130,7 +129,7 @@ export const work = {
 
 export const contact = {
   title: "Get in touch",
-  description: "Coffee Chat! Please do not hesitate to schedule a meeting. Alternatively, feel free to reach out directly by email at hashirshoaeb@gmail.com.",
+  description: "Please do not hesitate to reach out to collaborate, or tour the lab...",
   buttons: [
     {
       title: "Email Me",
