849ac7c77a66732b0ca2be4298b9bc896b7b92ff
Mohammad Alhussan <mohammad.alhussan.23@ucl.ac.uk>
2025-05-29 14:16:39 +0100

    Update news

--- _next/static/chunks/pages/news-837c570bcaab59b0.js
+++ _next/static/chunks/pages/news-837c570bcaab59b0.js
@@ -2,73 +2,57 @@
 
 l=[
   {
-    date: "25-28 May 2025 - ISCAS 2025 Presentation",
-    sortableDate: "2025-05-25",
-    title: "ISCAS 2025 Presentation",
-    description: "Mohammad Alhussan presented his work on BLE vulnerabilities in wearable sensor nodes at ISCAS 2025, London. The talk highlighted novel BLE threat models, experimental findings, and implications for securing medical wearables.",
-    image: "/ISCAS_images/2.jpg",
-    link: "/iscas.html"
+    date:"25-28 May 2025 - ISCAS 2025 Presentation",
+    description:"Mohammad Alhussan presented his work on BLE vulnerabilities in wearable sensor nodes at ISCAS 2025, London. The talk highlighted novel BLE threat models, experimental findings, and implications for securing medical wearables.",
+    image:"/ISCAS_images/2.jpg",
+    link:"/ISCAS.html"
   },
   {
-    date: "7th April 2025 - NTNU Lab Tour ",
-    sortableDate: "2025-04-07",
-    title: "NTNU Lab Tour",
-    description: "We hosted students and staff from the Norwegian University of Science and Technology's (NTNU) Cybersecurity program. The group engaged directly with our researchers developing revolutionary adaptive defense frameworks capable of evolving alongside emerging threats. ",
-    image: "/images/news/NTNUVisit.jpg",
-    link: ""
+    date:"7th April 2025 - NTNU Lab Tour ",
+    description:"We hosted students and staff from the Norwegian University of Science and Technology's (NTNU) Cybersecurity program. The group engaged directly with our researchers developing revolutionary adaptive defense frameworks capable of evolving alongside emerging threats. ",
+    image:"/images/news/NTNUVisit.jpg",
+    link:""
   },
   {
-    date: "15-16th March 2025 - RIPE NCC DNS Hackathon ",
-    sortableDate: "2025-03-15",
-    title: "RIPE NCC DNS Hackathon",
-    description: "PhD Student Andrew visited Stockholm for RIPE's DNS Hackathon. He formed a team made and developed code to investigate the IoT traffic captures. They uncovered critical DNS-related security vulnerabilities in IoT devices, discovering the highly fingerprintable nature of the traffic",
-    image: "/images/news/RIPENCC.png",
-    link: "https://labs.ripe.net/author/becha/join-the-dns-hackathon-2025/"
+    date:"15-16th March 2025 - RIPE NCC DNS Hackathon ",
+    description:"PhD Student Andrew visited Stockholm for RIPE's DNS Hackathon. He formed a team made and developed code to investigate the IoT traffic captures. They uncovered critical DNS-related security vulnerabilities in IoT devices, discovering the highly fingerprintable nature of the traffic",
+    image:"/images/news/RIPENCC.png",
+    link:"https://labs.ripe.net/author/becha/join-the-dns-hackathon-2025/"
   },
   {
-    date: "6th February 2025 - BBC's Blue Room Visit",
-    sortableDate: "2025-02-06",
-    title: "BBC's Blue Room Visit",
-    description: "We recieved an invitation to visit the BBC's 'Blue Room' - the consumer technology research and demo lab from BBC Research & Development. We discussed our work on Smart TVs as well as our Matter Device study and the security of medical devices",
-    image: "/images/news/BBCVisit.jpg",
-    link: "https://www.bbc.com/blueroom"
+    date:"6th February 2025 - BBC's Blue Room Visit",
+    description:"We recieved an invitation to visit the BBC's 'Blue Room' - the consumer technology research and demo lab from BBC Research & Development. We discussed our work on Smart TVs as well as our Matter Device study and the security of medical devices",
+    image:"/images/news/BBCVisit.jpg",
+    link:"https://www.bbc.com/blueroom"
   },
   {
-    date: "31st January 2025 - Visiting Speaker Michele Chiappalone",
-    sortableDate: "2025-01-31",
-    title: "Visiting Speaker Michele Chiappalone",
-    description: "We were thrilled to welcome Michele Chiappalone from Google to our Security and Privacy course at UCL Electronic and Electrical Engineering! With deep expertise in cloud, Michele shared insights into the latest challenges, best practices, and innovations in cloud environments. This is a fantastic opportunity for our students to gain real-world knowledge from an industry leader at the forefront.",
-    image: "/images/news/googleTalk.jpg",
-    link: "https://www.linkedin.com/posts/anna-maria-mandalari_cloudsecurity-cybersecurity-privacy-activity-7291073049213190144-tmhV?utm_source=share&utm_medium=member_desktop"
+    date:"31st January 2025 - Visiting Speaker Michele Chiappalone",
+    description:"We were thrilled to welcome Michele Chiappalone from Google to our Security and Privacy course at UCL Electronic and Electrical Engineering! With deep expertise in cloud, Michele shared insights into the latest challenges, best practices, and innovations in cloud environments. This is a fantastic opportunity for our students to gain real-world knowledge from an industry leader at the forefront.",
+    image:"/images/news/googleTalk.jpg",
+    link:"https://www.linkedin.com/posts/anna-maria-mandalari_cloudsecurity-cybersecurity-privacy-activity-7291073049213190144-tmhV?utm_source=share&utm_medium=member_desktop"
   },
   {
-    date: "8th January 2025 -  London Networks & Systems Symposium",
-    sortableDate: "2025-01-08",
-    title: "London Networks & Systems Symposium",
-    description: "Prof. Anna Mandalari gave a talk at Imperial College London's London Networks & Systems Symposium, titled 'Towards IoT Privacy and Security Verification'.",
-    image: "/images/news/imperialSymposium.jpeg",
-    link: "https://ix.imperial.ac.uk/event/london-networks-systems-symposium/"
+    date:"8th January 2025 -  London Networks & Systems Symposium",
+    description:"Prof. Anna Mandalari gave a talk at Imperial College London's London Networks & Systems Symposium, titled 'Towards IoT Privacy and Security Verification'.",
+    image:"/images/news/imperialSymposium.jpeg",
+    link:"https://ix.imperial.ac.uk/event/london-networks-systems-symposium/"
   },
   {
-    date: "10th -13th December 2024: The 21st International Conference on Embedded Wireless Systems and Networks (EWSN 2024)",
-    sortableDate: "2024-12-10",
-    title: "EWSN 2024 Best Demo Award",
-    description: "Prof. Anna Mandalari and PhD Students Mohammad Alhussan and Andrew Losty attended the 21st International Conference on Embedded Wireless Systems and Networks (EWSN 2024). Muhammad won the Best Demo Award for research on the potential vulnerabilities in wearable medical devices.",
-    image: "/images/news/bestDemo.jpeg",
-    link: "https://www.ucl.ac.uk/electronic-electrical-engineering/news/2024/dec/phd-student-wins-best-demonstration-award-ewsn-2024"
+    date:"10th -13th December 2024: The 21st International Conference on Embedded Wireless Systems and Networks (EWSN 2024)",
+    description:"Prof. Anna Mandalari and PhD Students Mohammad Alhussan and Andrew Losty attended the 21st International Conference on Embedded Wireless Systems and Networks (EWSN 2024). Muhammad won the Best Demo Award for research on the potential vulnerabilities in wearable medical devices.",
+    image:"/images/news/bestDemo.jpeg",
+    link:"https://www.ucl.ac.uk/electronic-electrical-engineering/news/2024/dec/phd-student-wins-best-demonstration-award-ewsn-2024"
   },
   {
-    date: "26th November 2024: Connected Ecosystems: Security and Resilience in IoT",
-    sortableDate: "2024-11-26",
-    title: "Connected Ecosystems Event",
-    description: "Prof. Anna Mandalari hosted a panel bringing together cybersecurity experts, industry leaders, and policymakers to explore emerging regulations, best practices, and innovative approaches to safeguarding IoT.",
-    image: "/images/news/august-2023.jpg",
-    link: "https://www.digicatapult.org.uk/apply/events/connected-ecosystems-security-and-resilience-in-iot/#:~:text=This%20event%20brings%20together%20cybersecurity,security%20standardisation%20and%20implementation%20strategies."
+    date:"26th November 2024: Connected Ecosystems: Security and Resilience in IoT",
+    description:"Prof. Anna Mandalari hosted a panel bringing together cybersecurity experts, industry leaders, and policymakers to explore emerging regulations, best practices, and innovative approaches to safeguarding IoT.",
+    image:"/images/news/august-2023.jpg",
+    link:"https://www.digicatapult.org.uk/apply/events/connected-ecosystems-security-and-resilience-in-iot/#:~:text=This%20event%20brings%20together%20cybersecurity,security%20standardisation%20and%20implementation%20strategies."
   }
 ];
 
 t.default=function(){
-  var e=(0,i.Z)(l).sort((function(e,t){return new Date(t.sortableDate)-new Date(e.sortableDate)}));
+  var e=(0,i.Z)(l).sort((function(e,t){return new Date(t.date)-new Date(e.date)}));
   return(0,a.jsxs)(s.Fragment,{children:[
     (0,a.jsx)(o.h,{seo:d.HJ}),
     (0,a.jsx)(c.J,{title:d.G.name,links:d.G.links}),
@@ -84,15 +68,15 @@ t.default=function(){
           ]})
         ]})})}),
         (0,a.jsx)("div",{className:"row",children:e.map((function(e,t){return(0,a.jsx)("div",{className:"col-md-6 col-lg-4 mb-4",children:(0,a.jsxs)("div",{className:"card h-100",children:[
-          (0,a.jsx)("img",{src:e.image,alt:e.title || e.date,className:"card-img-top",style:{height:"200px",objectFit:"cover"}}),
+          (0,a.jsx)("img",{src:e.image,alt:"News from ".concat(e.date),className:"card-img-top",style:{height:"200px",objectFit:"cover"}}),
           (0,a.jsxs)("div",{className:"card-body",children:[
             (0,a.jsx)("h5",{className:"card-title text-primary",children:e.date}),
             (0,a.jsx)("p",{className:"card-text text-primary",children:e.description}),
-            e.link ? (0,a.jsx)("a",{href:e.link,className:"btn btn-outline-primary",children:"Learn More"}) : null
+            (0,a.jsx)("a",{href:e.link,className:"btn btn-outline-primary",children:"Learn More"})
           ]})
         ]})},t)}))
       })
     ]}),
     (0,a.jsx)(r.$,{})
   ]})
-}}},function(e){e.O(0,[948,682,217,774,888,179],(function(){return t=5115,e(e.s=t);var t}));var t=e.O();_N_E=t}]);
+}}},function(e){e.O(0,[948,682,217,774,888,179],(function(){return t=5115,e(e.s=t);var t}));var t=e.O();_N_E=t;}]);
