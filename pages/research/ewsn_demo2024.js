f345b4456b8bc19f7abae6d4a5efb44b20bb7350
Ammara <AMMARA.YASIN.UCL@GMAIL.COM>
2025-07-07 18:40:30 +0100

    ignore apostrophe errors

--- pages/research/ewsn_demo2024.js
+++ pages/research/ewsn_demo2024.js
@@ -1,4 +1,5 @@
 // pages/research/ewsn-demo.js
+/* eslint-disable react/no-unescaped-entities */
 import Link from 'next/link';
 import { Footer } from '../../components/Footer';
 import { Header } from '../../components/Header';
