a1a2ea8ab56bf49ad0cb15d13b75ede67e7c41c8
Andrew Losty <68070479+AndrewLosty@users.noreply.github.com>
2025-08-28 10:44:50 +0100

    Update news.js

--- pages/news.js
+++ pages/news.js
@@ -1,4 +1,4 @@
-Fimport React, { Fragment } from 'react';
+import React, { Fragment } from 'react';
 import { Footer } from '../components/Footer';
 import { Header } from '../components/Header';
 import { Nav } from '../components/Navbar';
