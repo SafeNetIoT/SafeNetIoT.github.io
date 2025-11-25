40679cf02ba88a06b61179c31541be0b70bba05d
Andrew Losty <68070479+AndrewLosty@users.noreply.github.com>
2025-11-25 09:32:14 +0000

    Update news.js

--- pages/news.js
+++ pages/news.js
@@ -20,8 +20,7 @@ export const allNews = [
   description: "PhD candidate Andrew Losty was selected as part of the RACI (RIPE Academic Cooperation Initiative) to present his research at the RIPE 91 Meeting in Bucharest, Romania. He delivered two research presentations. In the Main Plenary Session, he presented *Towards Operational and Security Best Practices for DNS in the Internet of Things*, outlining IoT DNS research and progress towards adoption of the IETF BCP *IoT DNS Security and Privacy Guidelines*, a collaborative effort between UCL, INRIA and Infoblox. In the IoT Workshop, he presented *Beyond the Hype: Investigating Matter Standard’s Security and Privacy*, providing an update on the Matter standard, a security appraisal, and research in progress. ",
   image: "images/news/Andrew-raci-1.png",
   link: "https://ripe91.ripe.net/",
-  imagePosition: 'center 10%'
-},
+  },
   
   {
     date: "19-25th July 2025 - ANRW Workshop + IETF 123 meeting Madrid",
