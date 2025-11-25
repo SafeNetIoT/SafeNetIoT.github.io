d95166b14c4a8c4cee9d9392494c5f6b77a22df2
Andrew Losty <68070479+AndrewLosty@users.noreply.github.com>
2025-11-25 10:21:15 +0000

    Refactor RIPE 91 Meeting description with HTML
    Updated the description for the RIPE 91 Meeting to include HTML formatting and improved readability.

--- pages/news.js
+++ pages/news.js
@@ -17,10 +17,25 @@ export const allNews = [
 
 {
   date: "20–24 Oct 2024 – RIPE 91 Meeting Bucharest",
-  description: "PhD candidate Andrew Losty was selected as part of the RACI (RIPE Academic Cooperation Initiative) to present his research at the RIPE 91 Meeting in Bucharest, Romania. He delivered two research presentations. In the Main Plenary Session, he presented *Towards Operational and Security Best Practices for DNS in the Internet of Things*, outlining IoT DNS research and progress towards adoption of the IETF BCP *IoT DNS Security and Privacy Guidelines*, a collaborative effort between UCL, INRIA and Infoblox. In the IoT Workshop, he presented *Beyond the Hype: Investigating Matter Standard’s Security and Privacy*, providing an update on the Matter standard, a security appraisal, and research in progress. ",
+  description: `
+    PhD candidate Andrew Losty was selected as part of the RACI (RIPE Academic Cooperation Initiative) 
+    to present his research at the RIPE 91 Meeting in Bucharest, Romania. He delivered two research 
+    presentations.<br><br>
+
+    <strong>Main Plenary Session</strong><br>
+    *Towards Operational and Security Best Practices for DNS in the Internet of Things*, outline IoT DNS 
+    research and progress towards adoption of the IETF BCP *IoT DNS Security and Privacy Guidelines* 
+    (UCL, INRIA and Infoblox).<br><br>
+
+    <strong>IoT Workshop</strong><br>
+    *Beyond the Hype: Investigating Matter Standard’s Security and Privacy*, provide an update on the 
+    Matter standard, a security appraisal, and research in progress.
+  `,
   image: "images/news/andrew1.png",
   link: "https://ripe91.ripe.net/",
-  },
+},
+
+
   
   {
     date: "19-25th July 2025 - ANRW Workshop + IETF 123 meeting Madrid",
