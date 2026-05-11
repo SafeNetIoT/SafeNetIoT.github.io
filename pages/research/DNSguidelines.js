4aa9d784aeca734f3a953a4afc4561d5598f4cd7
Andrew Losty <68070479+AndrewLosty@users.noreply.github.com>
2026-05-11 14:59:20 +0100

    Refactor DNS guidelines images layout

--- pages/research/DNSguidelines.js
+++ pages/research/DNSguidelines.js
@@ -49,56 +49,57 @@ export default function DNSGuidelines() {
                 <strong>DOI:</strong> 10.1109/JIOT.2026.3680083
               </p>
 
-<div className="mt-5 text-center">
+                  
 
-  <img
-    src={dnsRetryImage.src}
-    alt="DNS Retry Guidelines"
-    className="img-fluid border border-secondary"
-    style={{
-      maxWidth: '750px',
-      width: '100%',
-      height: 'auto',
-      display: 'block',
-      margin: '0 auto'
-    }}
-  />
-  <p className="text-center font-italic mt-2">
-    DNS Retry Guidelines
-  </p>
+     <div className="mt-5">
+  <div className="row g-4 text-center">
 
-  <img
-    src={dnsTtlImage.src}
-    alt="DNS TTL Guidelines"
-    className="img-fluid border border-secondary mt-4"
-    style={{
-      maxWidth: '750px',
-      width: '100%',
-      height: 'auto',
-      display: 'block',
-      margin: '0 auto'
-    }}
-  />
-  <p className="text-center font-italic mt-2">
-    DNS TTL Guidelines
-  </p>
+    {/* Image 1 */}
+    <div className="col-12 col-md-4">
+      <img
+        src={dnsRetryImage.src}
+        alt="DNS Retry Guidelines"
+        className="img-fluid border border-secondary"
+        style={{
+          width: '100%',
+          maxHeight: '260px',
+          objectFit: 'contain'
+        }}
+      />
+      <p className="font-italic mt-2">DNS Retry Guidelines</p>
+    </div>
 
-  <img
-    src={dnsIqtImage.src}
-    alt="DNS IQT Guidelines"
-    className="img-fluid border border-secondary mt-4"
-    style={{
-      maxWidth: '750px',
-      width: '100%',
-      height: 'auto',
-      display: 'block',
-      margin: '0 auto'
-    }}
-  />
-  <p className="text-center font-italic mt-2">
-    DNS IQT Guidelines
-  </p>
+    {/* Image 2 */}
+    <div className="col-12 col-md-4">
+      <img
+        src={dnsTtlImage.src}
+        alt="DNS TTL Guidelines"
+        className="img-fluid border border-secondary"
+        style={{
+          width: '100%',
+          maxHeight: '260px',
+          objectFit: 'contain'
+        }}
+      />
+      <p className="font-italic mt-2">DNS TTL Guidelines</p>
+    </div>
 
+    {/* Image 3 */}
+    <div className="col-12 col-md-4">
+      <img
+        src={dnsIqtImage.src}
+        alt="DNS IQT Guidelines"
+        className="img-fluid border border-secondary"
+        style={{
+          width: '100%',
+          maxHeight: '260px',
+          objectFit: 'contain'
+        }}
+      />
+      <p className="font-italic mt-2">DNS IQT Guidelines</p>
+    </div>
+
+  </div>
 </div>
 
 
